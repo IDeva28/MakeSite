260cfd48b0efe38a2feb9373cbf58e0e19dcf0c8
Devarshee Thokal <72601128+IDeva28@users.noreply.github.com>
2021-07-19 14:36:33 +0530

    Creating components

---
+++ Javascript/ComponentForm.js
@@ -0,0 +1,27 @@
+function openSection(evt, SectionName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(SectionName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+  
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+  document.onload(on());
+  
+  
